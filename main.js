8d19a6a6a793ce271011f6c5b82ed2fc0acfa313
Alexandr <pochta@gmail.com>
2021-05-29 15:08:51 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+// 'use strict';
+// alert("ПРИВЕТ");
+
+// let cot = confirm('hello?', '');
+// console.log(cot);
+
+// let answer = prompt('Сколько вам лет?', ' 18');
+// console.log(typeof(answer));
+// console.log(answer);
+
+// let answers = [];
+// answers[0] = prompt('Сколько вам лет?', ' 18');
+// answers[1] = prompt('Как вас зовут?', ' ');
+// answers[2] = prompt('Из какого вы города?', ' ');
+
+// document.write(answers);
+// console.log(answers);
+
+// 9. Практика, ч.1. Начинаем создавать приложение
+// let numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
+// // let personalMovieDB = prompt('Один из последних просмотренных фильмов?', '');
+
+
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+// const a = prompt('Один из последних просмотренных фильмов?'),
+//     b = +prompt('На сколько оцените его?'),
+//     c = prompt('Один из последних просмотренных фильмов?'),
+//     d = +prompt('На сколько оцените его?');
+
+//     personalMovieDB.movies[a] = b;
+//     personalMovieDB.actors[c] = d;
+
+// console.log(personalMovieDB);
+
+
+// 10. Условия.mp4
+ 
+// if (0) {
+//     console.log('ok');
+// } else {
+//     console.log('error');
+
+// }
+
+
+// 11. Циклы.mp4
+
+
+
+
+
+
+
+
+
+
+
+
